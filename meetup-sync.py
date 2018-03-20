4620f25bd27b0072a79d15d867cbe9d773d908bd
Sean Dague <sean@dague.net>
2018-03-20 09:07:37 -0400

    fix indentation

--- meetup-sync.py
+++ meetup-sync.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# -*- mode: python-mode; python-indent-offset: 4 -*-
+#
+# Because my emacs session went nuts and started indenting 5
 
 import configparser
 import datetime
@@ -28,170 +31,175 @@ ADDRESS = """
 {city}, {state} {zipcode}
 """.replace("\n", "  \n")
 
+
 class Location(object):
-     def __init__(self, loc):
-          self._loc = loc
+    def __init__(self, loc):
+        self._loc = loc
 
-     def _g(self, name):
-          try:
-               return self._loc[name]
-          except KeyError:
-               return ""
+    def _g(self, name):
+        try:
+            return self._loc[name]
+        except KeyError:
+            return ""
 
-     @property
-     def title(self):
-          return self._g("title")
+    @property
+    def title(self):
+        return self._g("title")
 
-     @property
-     def address(self):
-          addr = self._g("address")
-          if self._g("address1"):
-               addr += "  \n" + self._g("address1")
+    @property
+    def address(self):
+        addr = self._g("address")
+        if self._g("address1"):
+            addr += "  \n" + self._g("address1")
 
-          return ADDRESS.format(
-               title=self._g("title"),
-               subtitle=self._g("subtitle"),
-               addr=addr,
-               city=self._g("city"),
-               state=self._g("state"),
-               zipcode=self._g("zip")
-          )
+        return ADDRESS.format(
+            title=self._g("title"),
+            subtitle=self._g("subtitle"),
+            addr=addr,
+            city=self._g("city"),
+            state=self._g("state"),
+            zipcode=self._g("zip")
+        )
 
 
 def find_location(name):
-     for root, dirs, files in os.walk("_locations"):
-          for f in files:
-               location = frontmatter.load(os.path.join(root, f))
-               print(location["title"])
-               if location["title"] == name:
-                    return Location(location)
-     raise Exception("No location found for {}".format(name))
+    for root, dirs, files in os.walk("_locations"):
+        for f in files:
+            location = frontmatter.load(os.path.join(root, f))
+            print(location["title"])
+            if location["title"] == name:
+                return Location(location)
+    raise Exception("No location found for {}".format(name))
 
 
 class Post(object):
 
-     def __init__(self, post):
-          self._post = post
-          self.start = datetime.datetime.strptime(post["dtstart"], "%Y-%m-%d %H:%M:%S %z")
-          self.end = datetime.datetime.strptime(post["dtend"], "%Y-%m-%d %H:%M:%S %z")
-          self.location = find_location(self._post["location"])
+    def __init__(self, post):
+        self._post = post
+        self.start = datetime.datetime.strptime(
+            post["dtstart"], "%Y-%m-%d %H:%M:%S %z")
+        self.end = datetime.datetime.strptime(
+            post["dtend"], "%Y-%m-%d %H:%M:%S %z")
+        self.location = find_location(self._post["location"])
 
-     @property
-     def description(self):
-          tags = ("h1", "h2", "h3", "h4")
-          location = LOCATION.format(self.location.address)
+    @property
+    def description(self):
+        tags = ("h1", "h2", "h3", "h4")
+        location = LOCATION.format(self.location.address)
 
-          content = self._post.content + location
+        content = self._post.content + location
 
-          desc = markdown.markdown(content)
-          for tag in tags:
-               desc = desc.replace("<{}>".format(tag), "</p><p>")
-               desc = desc.replace("</{}>".format(tag), ":</p>")
+        desc = markdown.markdown(content)
+        for tag in tags:
+            desc = desc.replace("<{}>".format(tag), "</p><p>")
+            desc = desc.replace("</{}>".format(tag), ":</p>")
 
-          # format out li
-          desc = desc.replace("<li>", "* ")
-          desc = desc.replace("</li>", "<br/>")
-          # strip out ul
-          desc = desc.replace("<ul>", "<p>")
-          desc = desc.replace("</ul>", "</p>")
-          return desc
+        # format out li
+        desc = desc.replace("<li>", "* ")
+        desc = desc.replace("</li>", "<br/>")
+        # strip out ul
+        desc = desc.replace("<ul>", "<p>")
+        desc = desc.replace("</ul>", "</p>")
+        return desc
 
-     @property
-     def time(self):
-          epoch = int(self.start.strftime('%s'))
-          return epoch * 1000
+    @property
+    def time(self):
+        epoch = int(self.start.strftime('%s'))
+        return epoch * 1000
 
-     @property
-     def duration(self):
-          delta = self.end - self.start
-          return delta.seconds * 1000
+    @property
+    def duration(self):
+        delta = self.end - self.start
+        return delta.seconds * 1000
 
-     @property
-     def title(self):
-          return self._post["title"]
+    @property
+    def title(self):
+        return self._post["title"]
 
-     @property
-     def meetup_id(self):
-          try:
-               meetup_id = self._post["meetup_id"]
-          except KeyError:
-               meetup_id = None
-          return meetup_id
+    @property
+    def meetup_id(self):
+        try:
+            meetup_id = self._post["meetup_id"]
+        except KeyError:
+            meetup_id = None
+        return meetup_id
 
-     @meetup_id.setter
-     def meetup_id(self, value):
-          self._post["meetup_id"] = str(value)
+    @meetup_id.setter
+    def meetup_id(self, value):
+        self._post["meetup_id"] = str(value)
 
-     def write(self, fname):
-          with open(fname, "w") as f:
-               f.write(frontmatter.dumps(self._post))
+    def write(self, fname):
+        with open(fname, "w") as f:
+            f.write(frontmatter.dumps(self._post))
 
 
 def get_meetup_key():
-     c = configparser.ConfigParser()
-     c.read("config.ini")
-     return c["default"].get("meetup_api_key")
+    c = configparser.ConfigParser()
+    c.read("config.ini")
+    return c["default"].get("meetup_api_key")
 
 
 def create_meetup(post):
-     data = {
-          "time": post.time,
-          "key": get_meetup_key(),
-          "sign": "true",
-          "group_urlname": GROUP,
-          "name": post.title,
-          "description": post.description,
-          "duration": post.duration
-     }
-     print(data)
-     res = requests.post(URL, data=data)
-     print("Status...")
-     print(res.status_code)
-     return json.loads(res.content.decode('utf-8'))
+    data = {
+        "time": post.time,
+        "key": get_meetup_key(),
+        "sign": "true",
+        "group_urlname": GROUP,
+        "name": post.title,
+        "description": post.description,
+        "duration": post.duration
+    }
+    print(data)
+    res = requests.post(URL, data=data)
+    print("Status...")
+    print(res.status_code)
+    return json.loads(res.content.decode('utf-8'))
 
 
 def update_meetup(post):
-     url = URL + "/" + post.meetup_id
-     data = {
-          "time": post.time,
-          "key": get_meetup_key(),
-          "sign": "true",
-          "group_urlname": GROUP,
-          "name": post.title,
-          "description": post.description,
-          "duration": post.duration
-     }
-     print(data)
-     res = requests.post(url, data=data)
-     print("Status...")
-     print(res.status_code)
-     return json.loads(res.content.decode('utf-8'))
+    url = URL + "/" + post.meetup_id
+    data = {
+        "time": post.time,
+        "key": get_meetup_key(),
+        "sign": "true",
+        "group_urlname": GROUP,
+        "name": post.title,
+        "description": post.description,
+        "duration": post.duration
+    }
+    print(data)
+    res = requests.post(url, data=data)
+    print("Status...")
+    print(res.status_code)
+    return json.loads(res.content.decode('utf-8'))
+
 
 def all_events():
-     events = []
+    events = []
 
-     for root, dirs, files in os.walk("_events"):
-          for f in files:
-               if f.endswith(".md"):
-                    events.append(os.path.join(root, f))
-     return events
+    for root, dirs, files in os.walk("_events"):
+        for f in files:
+            if f.endswith(".md"):
+                events.append(os.path.join(root, f))
+    return events
+
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('event', type=click.Path(exists=True), required=True)
 def main(event):
-     """Sync events to meetup via the api
+    """Sync events to meetup via the api
 
-     The EVENT is the path to a file in the _events tree which will be
-     synced to meetup.
+    The EVENT is the path to a file in the _events tree which will be
+    synced to meetup.
 
-     """
-     post = Post(frontmatter.load(event))
-     if post.meetup_id is None:
-          resp = create_meetup(post)
-          post.meetup_id = resp["id"]
-          post.write(event)
-     else:
-          resp = update_meetup(post)
+    """
+    post = Post(frontmatter.load(event))
+    if post.meetup_id is None:
+        resp = create_meetup(post)
+        post.meetup_id = resp["id"]
+        post.write(event)
+    else:
+        resp = update_meetup(post)
 
 
 if __name__ == '__main__':

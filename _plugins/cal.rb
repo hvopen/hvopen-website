52d470f8aae16366bdfcf629847929e5971feb48
Sean Dague <sean@dague.net>
2018-03-26 06:16:54 -0400

    Add ical generator

---
+++ _plugins/cal.rb
@@ -0,0 +1,49 @@
+require 'icalendar'
+
+module Jekyll
+
+  class CalendarGenerator < Generator
+    safe true
+
+    def generate(site)
+      cal = Icalendar::Calendar.new
+      cal.timezone do |t|
+        t.tzid = "America/New_York"
+
+        t.daylight do |d|
+          d.tzoffsetfrom = "-0500"
+          d.tzoffsetto   = "-0400"
+          d.tzname       = "EDT"
+          d.dtstart      = "19700308T020000"
+          d.rrule        = "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
+        end
+
+        t.standard do |s|
+          s.tzoffsetfrom = "-0400"
+          s.tzoffsetto   = "-0500"
+          s.tzname       = "EST"
+          s.dtstart      = "19701101T020000"
+          s.rrule        = "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
+        end
+      end
+
+      events = site.collections["events"]
+
+      events.docs.each do |event|
+        cal.event do |e|
+          e.dtstart = Icalendar::Values::DateTime.new event.dtstart, 'tzid' => "America/New_York"
+          e.dtend = Icalendar::Values::DateTime.new event.dtend, 'tzid' => "America/New_York"
+          e.summary = event.title
+        end
+      end
+
+
+      file = File.new(File.join(site.source, "events.ics"), "w")
+      puts cal.to_ical
+      file.write(cal.to_ical)
+      file.close()
+
+      site.static_files << Jekyll::StaticFile.new(site, site.source, "/", "events.ics")
+    end
+  end
+end

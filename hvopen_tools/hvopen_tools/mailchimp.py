b70be93f4d0404fac09caefdf17130d84cc7c50b
Sean Dague <sean@dague.net>
2018-09-24 07:39:36 -0400

    add button creator

---
+++ hvopen_tools/hvopen_tools/mailchimp.py
@@ -0,0 +1,27 @@
+
+
+BUTTON_MARKUP = """
+<table class="mcnButtonBlock" style="min-width: 100%;border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;" width="100%" cellspacing="0" cellpadding="0" border="0">
+    <tbody class="mcnButtonBlockOuter">
+        <tr>
+            <td style="padding-top: 0;padding-right: 18px;padding-bottom: 18px;padding-left: 18px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;" class="mcnButtonBlockInner" valign="top" align="center">
+                <table class="mcnButtonContentContainer" style="border-collapse: separate !important;border-radius: 3px;background-color: {bgcolor};mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;" cellspacing="0" cellpadding="0" border="0">
+                    <tbody>
+                        <tr>
+                            <td class="mcnButtonContent" style="font-family: Helvetica;font-size: 18px;padding: 18px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;" valign="middle" align="center">
+                                <a class="mcnButton " title="{text}" href="{url}" target="_blank" style="font-weight: bold;letter-spacing: -0.5px;line-height: 100%;text-align: center;text-decoration: none;color: {color};mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;display: block;">{text}</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>"""
+
+def mailchimp_button(text, url, bgcolor, color="#FFFFFF"):
+    return BUTTON_MARKUP.format({
+        "text": text,
+        "url": url,
+        "bgcolor": bgcolor,
+        "color": color})
